32e6da616609d04b0b8eaf13eca169b176f351c9
landon hickman <landonhickman12@gmail.com>
2021-06-22 15:11:12 -0600

    seeded and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
 Test.destroy_all
+Buyer.destroy_all
+Product.destroy_all
+Seller.destroy_all
 
-Test.create(name: 'Test1')
-Test.create(name: 'Test2')
-Test.create(name: 'Test3')
-Test.create(name: 'Test4')
-Test.create(name: 'Test5')
+cat = [
+  'Outdoors & Gardening',
+  'Crafts',
+  'Clothing',
+  'Toys',
+  'Art',
+  'Gifts'
+]
 
-puts 'Seeded 5'
+5.times do
+ seller = Seller.create(name: Faker::Name.name, email: Faker::Internet.email)
+ 5.times do
+  num_cat = rand(0..cat.length-1)
+  seller.buyers.create(name: Faker::Name.name, max_price: rand(50...400), desired_categories: cat.sample(num_cat))
+ end
+ 5.times do
+  seller.products.create(name: Faker::Food.fruits, price: rand(15...400), description: Faker::Food.description, category: cat.sample)
+ end
+end
+
+puts "seeded Seller #{Seller.all.length}"
+puts "seeded Buyer #{Buyer.all.length}"
+puts "seeded Product #{Product.all.length}"
