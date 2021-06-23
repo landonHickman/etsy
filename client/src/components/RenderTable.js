3a17a39123f7cebc65ea32faf96265e65476d003
landon hickman <landonhickman12@gmail.com>
2021-06-23 16:43:42 -0600

    render products page by seller

--- client/src/components/RenderTable.js
+++ client/src/components/RenderTable.js
@@ -1,47 +1,36 @@
 import React from "react";
-import { Table } from "semantic-ui-react";
+import { Table} from "semantic-ui-react";
 
 //pass in data i.e. table={data} then it outputs a mapped over array in jsx.
-const RenderTable = ({ table }) => {
-  console.log("table", table);
-
-  
-
-  const renderT = () => {
-    return table.map((d) => {
+const RenderTable = (props) => {
+  const {seller_name, seller_email, products} = props
       
-        return (
-          <div key={d.product_id}>
-            <h4>{d.seller_name + ` ` + d.seller_email}</h4>
-            <Table fixed>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell>Name</Table.HeaderCell>
-                  <Table.HeaderCell>Description</Table.HeaderCell>
-                  <Table.HeaderCell>Price</Table.HeaderCell>
-                  <Table.HeaderCell>Category</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-    
-              <Table.Body>
-                <Table.Row>
-                  <Table.Cell>{d.product_name}</Table.Cell>
-                  <Table.Cell>{d.description}</Table.Cell>
-                  <Table.Cell>{d.price} </Table.Cell>
-                  <Table.Cell>{d.category} </Table.Cell>
-                </Table.Row>
-              </Table.Body>
-            </Table>
-          </div>
-        );
-    });
-  }
-  
   return (
-    <div>
-      {renderT()}
-    </div>
-  )
-  
+    <>
+      <h1>{seller_name}</h1>
+      <h2>{seller_email}</h2>
+      <Table fixed>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Item</Table.HeaderCell>
+            <Table.HeaderCell>Description</Table.HeaderCell>
+            <Table.HeaderCell>Price</Table.HeaderCell>
+            <Table.HeaderCell>Category</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {products.map(p=> (
+              <Table.Row key={p.product_id}>
+                <Table.Cell>{p.product_name}</Table.Cell>
+                <Table.Cell>{p.description}</Table.Cell>
+                <Table.Cell>{p.price}</Table.Cell>
+                <Table.Cell>{p.category}</Table.Cell>
+              </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </>
+  );
 };
 export default RenderTable;
--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,15 +1,67 @@
-import React from 'react'
-import useAxiosOnMount from '../customHooks/useAxiosOnMount'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import RenderTable from '../components/RenderTable'
 
 const Products = () => {
-  const {data} = useAxiosOnMount(`/api/products`)
+  const [products, setProducts] = useState([])
 
+  useEffect(()=>{
+    getProducts()
+  },[])
+
+  const normalizeProducts = (prod) => {
+
+    const sell_id = prod.map(p=> p.seller_id)
+
+    const uniqueId = [...new Set(sell_id)]
+
+    const productData = []
+
+    uniqueId.forEach(id => {
+
+      //creates a new array for each time ap.seller_id === id
+      let productsFiltered = prod.filter(ap => ap.seller_id === id)
+
+      const clean = productsFiltered.map(c=> {
+        return {
+          category: c.category,
+          description: c.description,
+          price: c.price,
+          product_id: c.product_id,
+          product_name: c.product_name,
+        }
+      })
+
+      let {seller_id, seller_name, seller_email } = productsFiltered[0]
+
+      productData.push({
+        seller_id,
+        seller_name,
+        seller_email,
+        products: clean
+      })
+    })
+    setProducts(productData)
+  }
+
+  const getProducts = async () => {
+    try{
+      let res = await axios.get(`/api/products`)
+      normalizeProducts(res.data)
+    }catch(err){
+      alert('err')
+      console.log(err)
+      console.log(err.response)
+    }
+  }
 
   return (
     <div>
-      <h1>Products</h1>
-      <RenderTable table={data}/>
+      {products.map(p=> {
+          return(
+            <RenderTable key={p.seller_id} {...p}/>
+          )
+      })}
     </div>
   )
 }
