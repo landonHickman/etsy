9f2529efa832c51dfbea0d6dd630d683f8867345
landon hickman <landonhickman12@gmail.com>
2021-06-24 16:54:50 -0600

    FE for dropdown for buyers

--- client/src/pages/FindProduct.js
+++ client/src/pages/FindProduct.js
@@ -4,15 +4,19 @@ import { Select } from 'semantic-ui-react'
 
 const FindProduct = () => {
   const [sellers, setSellers] = useState([])
+  const [buyers, setBuyers] = useState([])
+  const [showBuyers, setShowBuyers] = useState(false)
 
   useEffect(()=>{
-    getSeller()
+    axiosCalls()
   },[])
 
-  const getSeller = async () => {
+  const axiosCalls = async () => {
     try{
       let res = await axios.get(`/api/sellers`)
       setSellers(res.data)
+      let res1 = await axios.get('/api/buyers')
+      setBuyers(res1.data)
     }catch(err){
       console.log('err',err)
       console.log('err.response',err.response)
@@ -22,14 +26,25 @@ const FindProduct = () => {
   const sellerOptions = () => {
     return sellers.map(s => ({key: s.id, text: `${s.name} ${s.item_count} Items`, value: s.id }))
   }
-  const handleChange = () => {
-    //TODO:
+
+  const buyerOptions = () => {
+    return buyers.map(s => ({key: s.id, text: `${s.name}`, value: s.id }))
+  }
+
+  const handleSellerChange = async () => {
+    try{
+      setShowBuyers(!showBuyers) 
+    }catch(err){
+      console.log('err',err)
+    }
   }
 
   return (
     <div>
       <h1>FindProduct</h1>
-      <Select onChange={handleChange} placeholder='Select Seller' options={sellerOptions()}/>
+      <Select onChange={handleSellerChange} placeholder='Select Seller' options={sellerOptions()}/>
+      <br/>
+      {showBuyers && <Select placeholder='Select Seller' options={buyerOptions()}/>}
     </div>
   )
 }
